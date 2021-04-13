d66fcc9364709fea1938237d27bd573a11f5b5b3
Mzack9999 <mzack9999@protonmail.com>
2021-04-13 23:33:42 +0200

    origin

---
+++ stringsutil.go
@@ -0,0 +1,73 @@
+package stringutil
+
+import "strings"
+
+// https://www.dotnetperls.com/between-before-after-go
+
+func Between(value string, a string, b string) string {
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
+}
+
+func Before(value string, a string) string {
+	pos := strings.Index(value, a)
+	if pos == -1 {
+		return ""
+	}
+	return value[0:pos]
+}
+
+func After(value string, a string) string {
+	pos := strings.LastIndex(value, a)
+	if pos == -1 {
+		return ""
+	}
+	adjustedPos := pos + len(a)
+	if adjustedPos >= len(value) {
+		return ""
+	}
+	return value[adjustedPos:]
+}
+
+func HasPrefixAny(s string, prefixes ...string) bool {
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(s, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
+func HasSuffixAny(s string, suffixes ...string) bool {
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(s, suffix) {
+			return true
+		}
+	}
+	return false
+}
+
+func TrimPrefixAny(s string, prefixes ...string) string {
+	for _, prefix := range prefixes {
+		s = strings.TrimPrefix(s, prefix)
+	}
+	return s
+}
+
+func TrimSuffixAny(s string, suffixes ...string) string {
+	for _, suffix := range suffixes {
+		s = strings.TrimSuffix(s, suffix)
+	}
+	return s
+}
